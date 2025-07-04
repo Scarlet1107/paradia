f94a3f518b11753ca5f4fce68de63ac65f168ff6
scarlet_7 <shougo.ushiro@gmail.com>
2025-07-04 17:15:21 +0900

    fix import error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
-import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import DynamicBackground from "@/components/dynamicBackground";
