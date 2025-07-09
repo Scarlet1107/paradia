d9e519f4da6b8ffdd71c984d613eaa0dcd9bdbfd
scarlet_7 <shougo.ushiro@gmail.com>
2025-07-09 19:27:26 +0900

    darker bad end page

--- components/dynamicBackground.tsx
+++ components/dynamicBackground.tsx
@@ -1,8 +1,22 @@
 // components/DynamicBackground.tsx
 "use client";
 
+import { usePathname } from "next/navigation";
+
 export default function DynamicBackground() {
-  return (
-    <div className="fixed inset-0 -z-10 bg-[url('/background.png')] bg-cover bg-center bg-no-repeat" />
-  );
+  const pathName = usePathname();
+  const isBadEnd = pathName ? pathName.endsWith("/delete-user") : false;
+  console.log("DynamicBackground pathName:", pathName, "isBadEnd:", isBadEnd);
+
+  const renderBackground = () => {
+    return isBadEnd ? (
+      // ユーザー削除ページの時の背景
+      <div className="fixed inset-0 z-50 bg-gray-700/80 bg-cover bg-center bg-no-repeat" />
+    ) : (
+      // それ以外の背景
+      <div className="fixed inset-0 -z-10 bg-[url('/background.png')] bg-cover bg-center bg-no-repeat" />
+    );
+  };
+
+  return renderBackground();
 }
